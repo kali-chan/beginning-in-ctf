6437d9e96f60efe9dd8e15c73c3deab90e354fc6
kali chan <kali.chan.linux@gmail.com>
2023-09-25 18:08:54 +0900

    update

--- beginning.sh
+++ beginning.sh
@@ -1,13 +1,27 @@
 #!/bin/sh
 
-echo "Please enter atacking remote ip address:"
+# Ask user to enter an IP address
+echo "Please enter attacking remote IP address:"
 
 # Read user input into a variable
 read r_address
 
-sudo nmap -sV $r_address
+# Check if the user has entered a valid IP address
+if [[ $r_address =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+    echo "You have entered a valid IP address!"
 
-gobuster dir -u $r_address -w /usr/share/seclists/Discovery/Web-Content/big.txt
+    # Run nmap scan
+    echo "Running nmap scan..."
+    sudo nmap -sV $r_address
 
-echo "How was it?"
-echo "If it is not working, please concider using ZAP, SQLMAP, OpenVAS, Burpsuite..."
+    # Run gobuster
+    echo "Running gobuster..."
+    gobuster dir -u $r_address -w /usr/share/seclists/Discovery/Web-Content/big.txt
+
+    # Ask the user if the scan worked
+    echo "How was it?"
+    echo "If it is not working, please concider using ZAP, SQLMAP, OpenVAS, Burpsuite..."
+
+else
+    echo "You have entered an invalid IP address!"
+fi
